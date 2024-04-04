799af67c25cb0a30fb8f34279c9a0313f4c69bf9
chat1891 <chatss1891@gmail.com>
2024-04-03 21:30:02 -0400

    fix car spinning to stop after 100 steps

--- utils/envs.py
+++ utils/envs.py
@@ -29,6 +29,7 @@ def play_episode_rb(env, policy, buf):
     states, actions, rewards = [], [], []
     states.append(env.FirstStep())
     done = False
+    counterToDie =0
     while not done:
         action = policy(env, states[-1])
         actions.append(action)
@@ -36,6 +37,14 @@ def play_episode_rb(env, policy, buf):
         buf.add(states[-1], action, reward, obs, done)
         states.append(obs)
         rewards.append(reward)
+
+        #add counter to prevent is sping at one position not moving forward
+        if reward==0:
+            counterToDie+=1
+            if counterToDie>100:
+                done=True
+        else:
+            counterToDie=0 
     return states, actions, rewards
 
 
@@ -47,6 +56,7 @@ def play_episode(env, policy, render = True):
     states, actions, rewards = [], [], []
     states.append(env.FirstStep())
     done = False
+    counterToDie=0
     if render: env.render()
     while not done:
         action = policy(env, states[-1])
@@ -55,6 +65,14 @@ def play_episode(env, policy, render = True):
         if render: env.render()
         states.append(obs)
         rewards.append(reward)
+
+        #add counter to prevent is sping at one position not moving forward
+        if reward==0:
+            counterToDie+=1
+            if counterToDie>100:
+                done=True
+        else:
+            counterToDie=0 
     return states, actions, rewards
 
 # Play an episode according to a given policy and add 
